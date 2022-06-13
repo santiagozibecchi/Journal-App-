e2bc98700ebcdcacc1e0248583e1007b0bcdb6f2
Santiago <santiago.zibe@gmail.com>
2022-06-13 20:59:05 -0300

    mui

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,9 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const JournalPage = () => {
      return (
-          <div>JournalPage</div>
+          <Typography variant='h1' >JournalPage</Typography>
      )
 }
 
---
+++ src/theme/AppTheme.jsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { purpleTheme } from './purpleTheme';
+
+const AppTheme = ({ children }) => {
+     return (
+          <ThemeProvider theme={purpleTheme}>
+               {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+               <CssBaseline />
+               {children}
+          </ThemeProvider>
+     )
+}
+
+export default AppTheme;
