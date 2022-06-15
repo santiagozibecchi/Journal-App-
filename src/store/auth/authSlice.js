3fa05f5e055dd27612bbfade30bf347dd29993b5
Santiago <santiago.zibe@gmail.com>
2022-06-15 19:08:32 -0300

    useForm

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,26 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-         status: 'checking', // 'not-authenticated', 'authenticated'
-         uid: null,
-         email: null,
-         displayName: null,
-         photoURL: null,
+     name: 'auth',
+     initialState: {
+          status: 'not-authenticated', // 'not-authenticated', 'authenticated', 'checking'
+          uid: null,
+          email: null,
+          displayName: null,
+          photoURL: null,
           errorMessage: null,
-    },
-    reducers: {
-         login: (state, action) => {
+     },
+     reducers: {
+          login: (state, action) => {
 
-         },
-         logout: (state, payload) => {
+          },
+          logout: (state, payload) => {
 
-         },
-         checkingCredentials: (state) => {
+          },
+          checkingCredentials: (state) => {
 
-         },
-    },
+          },
+     },
 });
 
 
